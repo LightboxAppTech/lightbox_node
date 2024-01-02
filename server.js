c52ad98ca83cd5ce9aa8f330fc7562adde4681cb
NisargChokshi45 <nisarg.chokshi@bacancy.com>
2024-01-02 23:39:24 +0530

    Add express-rate-limit node module

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const http = require('http')
 const helmet = require('helmet')
+const { rateLimit } = require('express-rate-limit')
 const { initSocket } = require('./utility/socket')
 const port = process.env.PORT || 3000
 
@@ -13,8 +14,16 @@ const app = express()
 const server = http.createServer(app)
 const socketio = require('socket.io').listen(server)
 
+const requestLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000,
+	limit: 100, // Limit each IP to 100 requests per 'window'(5 minutes).
+	standardHeaders: 'draft-7', // draft-6: 'RateLimit-*' headers; draft-7: combined 'RateLimit' header
+	legacyHeaders: false, // Disable the 'X-RateLimit-*' headers.
+})
+
 dotenv.config()
 app.use(helmet())
+app.use(requestLimiter)
 app.use(cookieParser())
 app.use(express.json({ limit: '100MB' }))
 app.use(cors({ credentials: true, origin: true }))

be85a9cc72e60194bb5c9018582438f4373f6dd0
Nisarg Chokshi <101206240+Nisarg-Chokshi@users.noreply.github.com>
2024-01-20 22:10:50 +0530

    Fix the project setup configuration issues (#3)
    * Remove body-parser node module
    
    * Remove express-limiter node module
    
    * Add express-rate-limit node module
    
    * Update joi node module dependency
    
    * Migrate code to aws-sdk v3
    
    * Upgrade cookie-parser node module
    
    * Upgrade dotenv node module
    
    * Upgrade express node module
    
    * Upgrade file-type node module
    
    * Upgrade googleapis node module
    
    * Upgrade helmet node module
    
    * Upgrade jsonwebtoken node module
    
    * Upgrade mongoose node module
    
    * Remove unused node modules
    
    * Upgrade nodemailer node module
    
    * Upgrade sharp node module
    
    * Upgrade socket.io node module
    
    * Upgrade stopword node module
    
    * Revert socket.io node module version
    
    * Add package-lock.json file
    
    * Add a few tasks as the todo list
    
    ---------
    
    Co-authored-by: NisargChokshi45 <nisarg.chokshi@bacancy.com>

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const routes = require('./routes/routes')
@@ -7,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const http = require('http')
 const helmet = require('helmet')
-const limiter = require('express-limiter')
+const { rateLimit } = require('express-rate-limit')
 const { initSocket } = require('./utility/socket')
 const port = process.env.PORT || 3000
 
@@ -15,10 +14,18 @@ const app = express()
 const server = http.createServer(app)
 const socketio = require('socket.io').listen(server)
 
+const requestLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000,
+	limit: 100, // Limit each IP to 100 requests per 'window'(5 minutes).
+	standardHeaders: 'draft-7', // draft-6: 'RateLimit-*' headers; draft-7: combined 'RateLimit' header
+	legacyHeaders: false, // Disable the 'X-RateLimit-*' headers.
+})
+
 dotenv.config()
 app.use(helmet())
+app.use(requestLimiter)
 app.use(cookieParser())
-app.use(bodyParser.json({ limit: '100MB' }))
+app.use(express.json({ limit: '100MB' }))
 app.use(cors({ credentials: true, origin: true }))
 app.set('io', socketio)
 initSocket(socketio)

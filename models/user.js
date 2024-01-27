eef8afcbb3259d0a4e2a0c0b496dad3314e3dc58
Nisarg-Chokshi <nisarg4000@gmail.com>
2024-01-27 19:37:10 +0530

    Rectify user model schema definition

--- models/user.js
+++ models/user.js
@@ -3,21 +3,19 @@ const { Boolean, Number, String } = mongoose.Schema.Types
 
 const VerificationSchema = new mongoose.Schema(
   {
-    code: { type: Number, required: true },
-    validUpTo: { type: Number, required: true },
+    code: { type: Number, default: null },
+    validUpTo: { type: Number, default: null },
   },
   { _id: false }
 )
-
-const defaultVerification = { code: null, validUpTo: null }
 
 const UserSchema = new mongoose.Schema(
   {
     email: { type: String, minlength: 8, maxlength: 100, unique: true, required: true },
     password: { type: String, minlength: 8, maxlength: 100, required: true },
     isVerified: { type: Boolean, default: false },
-    emailVerification: { type: VerificationSchema, default: defaultVerification },
-    forgotPasswordVerification: { type: VerificationSchema, default: defaultVerification },
+    emailVerification: { type: VerificationSchema },
+    forgotPasswordVerification: { type: VerificationSchema },
   },
   { timestamps: true }
 )

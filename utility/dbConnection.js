50c85bdd316f3eaca296f89d139fcb880553eb9b
Nisarg-Chokshi <nisarg4000@gmail.com>
2024-01-27 17:31:44 +0530

    Add custom logger in dbConnection

--- utility/dbConnection.js
+++ utility/dbConnection.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const logger = require('./logger')
+
 module.exports = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_CONNECTION, {
       useNewUrlParser: true,
     })
-    console.log('Connected to database')
+    logger.info('Connected to database')
   } catch (error) {
-    console.error('Error connecting to database =>', error.message)
-    process.exit(1)
+    logger.error('Error connecting to database =>', error)
+    throw new Error(error)
   }
 }
